381e7fd9971779014bf82ac0ee05f8738db6b10b
Matheus Acácio <matheusacacio@unigranrio.br>
2020-05-28 22:36:42 -0300

    Pefra Papel Tesoura com CSS

--- pedra-papel-tesoura/script.js
+++ pedra-papel-tesoura/script.js
@@ -1,28 +1,28 @@
 var placar_jogador = 0
 var placar_maquina = 0
+
 var txt_placar_jogador = document.querySelector('td#placarjogador')
 var txt_placar_maquina = document.querySelector('td#placarmaquina')
-
-var txt_jogador = document.querySelector('p#usuario')
-var txt_maquina = document.querySelector('p#maquina')
 var txt_resultado = document.querySelector('p#resultado')
+var img_jogador = document.querySelector('img#imagem-jogador')
+var img_oponente = document.querySelector('img#imagem-oponente')
 
 function PlayPedra() {
     var valor_jogador = 1
-    txt_jogador.innerHTML = 'voce escolheu pedra'
+    img_jogador.setAttribute('src', 'pedra-jogador.png')
     var valor_maquina = Math.floor(Math.random() * 3 + 1)
 
     if (valor_maquina == 1) {
-        txt_resultado.innerHTML = 'Empate'
-        txt_maquina.innerHTML = 'oponente escolheu pedra'
+        img_oponente.setAttribute('src', 'pedra-oponente.png')
+        txt_resultado.innerHTML = 'Empate!'
     } else if (valor_maquina == 2) {
+        img_oponente.setAttribute('src', 'papel-oponente.png')
+        txt_resultado.innerHTML = 'Oponente venceu.'
         placar_maquina++
-        txt_maquina.innerHTML = 'oponente escolheu papel'
-        txt_resultado.innerHTML = 'oponente venceu'
     } else {
+        img_oponente.setAttribute('src', 'tesoura-oponente.png')
+        txt_resultado.innerHTML = 'Você venceu!!'
         placar_jogador++
-        txt_maquina.innerHTML = 'oponente escolheu tesoura'
-        txt_resultado.innerHTML = 'você venceu'
     }
         
     txt_placar_jogador.innerHTML = `${placar_jogador}`
@@ -31,19 +31,19 @@ function PlayPedra() {
 
 function PlayPapel() {
     var valor_jogador = 2
-    txt_jogador.innerHTML = 'você escolheu papel'
+    img_jogador.setAttribute('src', 'papel-jogador.png')
     var valor_maquina = Math.floor(Math.random() * 3 + 1)
 
     if (valor_maquina == 1) {
-        txt_resultado.innerHTML = 'voce venceu'
-        txt_maquina.innerHTML = 'oponente escolheu pedra'
+        img_oponente.setAttribute('src', 'pedra-oponente.png')
+        txt_resultado.innerHTML = 'Você venceu!!'
         placar_jogador++
     } else if (valor_maquina == 2) {
-        txt_maquina.innerHTML = 'oponente escolheu papel'
-        txt_resultado.innerHTML = 'empate'
+        img_oponente.setAttribute('src', 'papel-oponente.png')
+        txt_resultado.innerHTML = 'Empate!'
     } else {
-        txt_maquina.innerHTML = 'oponente escolheu tesoura'
-        txt_resultado.innerHTML = 'oponente venceu'
+        img_oponente.setAttribute('src', 'tesoura-oponente.png')
+        txt_resultado.innerHTML = 'Oponente venceu.'
         placar_maquina++
     }
         
@@ -53,20 +53,21 @@ function PlayPapel() {
 
 function PlayTesoura() {
     var valor_jogador = 3
-    txt_jogador.innerHTML = 'voce escolheu tesoura'
+    img_jogador.setAttribute('src', 'tesoura-jogador.png')
     var valor_maquina = Math.floor(Math.random() * 3 + 1)
 
     if (valor_maquina == 1) {
-        txt_resultado.innerHTML = 'oponente venceu'
-        txt_maquina.innerHTML = 'oponente escolheu pedra'
+        img_oponente.setAttribute('src', 'pedra-oponente.png')
+        txt_resultado.innerHTML = 'Oponente venceu.'
         placar_maquina++
     } else if (valor_maquina == 2) {
-        txt_maquina.innerHTML = 'oponente escolheu papel'
-        txt_resultado.innerHTML = 'você venceu'
+        img_oponente.setAttribute('src', 'papel-oponente.png')
+        txt_resultado.innerHTML = 'Você venceu!!'
         placar_jogador++
     } else {
-        txt_maquina.innerHTML = 'oponente escolheu tesoura'
-        txt_resultado.innerHTML = 'empate'
+        img_oponente.setAttribute('src', 'tesoura-oponente.png')
+        txt_maquina.innerHTML = 'Oponente escolheu tesoura.'
+        txt_resultado.innerHTML = 'Empate!'
     }
         
     txt_placar_jogador.innerHTML = `${placar_jogador}`
